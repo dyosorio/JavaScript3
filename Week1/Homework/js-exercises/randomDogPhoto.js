08a1ec00994ceb3505c839994b589d035a39248d
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-08-27 20:28:22 +0200

    Exercise 3: improved functions. Status: Finished

--- Week1/Homework/js-exercises/randomDogPhoto.js
+++ Week1/Homework/js-exercises/randomDogPhoto.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable func-names */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-use-before-define */
@@ -14,7 +15,7 @@ Let's make a randomized dog photo gallery!
 
 Write a function that makes a HTTP Request to https://dog.ceo/api/breeds/image/random. 
 It should trigger after clicking a button in your webpage. 
-***Every time the button is clicked it should append a new dog image to the DOM.***
+Every time the button is clicked it should append a new dog image to the DOM
 
 Create an index.html file that will display your random image
 Add 2 <button> and 1 <ul> element, either in the HTML or through JavaScript
@@ -24,19 +25,19 @@ After receiving the data, append to the <ul> a <li> that contains an <img> eleme
 Incorporate error handling: log to the console the error message
 */
 
-// create the XHR-button event listener
+// create the xhr-button and axios-button event listeners
 document.getElementById('xhr-button').addEventListener('click', displayDogImageXHR);
 document.getElementById('axios-button').addEventListener('click', displayDogImageAxios);
 
-
+// style the container of both xhr and axios images
 const gallery = document.getElementById("gallery-container");
-gallery.style.backgroundColor = "#f7f1e3";
+gallery.style.display = "flex";
+gallery.style.backgroundColor = "#aaa69d";
 gallery.style.paddingTop = "1.5rem";
 gallery.style.paddingBottom = "1.5rem";
-gallery.style.display = "flex";
 
-
-function errorMessage(){
+// display error message for both xhr and axios functions
+function displayErrorMessage(){
     const errorMessage = document.createElement("p");
     errorMessage.innerHTML = "Request Failed!";
     errorMessage.style.textAlign = "center";
@@ -52,6 +53,31 @@ function errorMessage(){
     document.body.appendChild(errorImage);
 }
 
+// display image for both xhr and axios functions
+function displayImage(dogImage){
+    //create elements and add them to the DOM
+    const imageEl = document.createElement("img");
+    const divElement = document.getElementById("list-container");
+    const ulElement = document.getElementById("list");
+    const liElement = document.createElement("li");
+    divElement.appendChild(ulElement);
+    ulElement.appendChild(liElement);
+    liElement.appendChild(imageEl);
+
+    //style elements
+    imageEl.src = dogImage;
+    imageEl.style.border = "10px solid #fff";
+    imageEl.style.width = "50%";
+    imageEl.style.margin = "auto";
+    divElement.style.display = "flex";
+    divElement.style.justifyItems = "center";
+    divElement.style.alignContent = "center";
+    divElement.style.overflow = "hidden";
+    divElement.style.width = "100%";
+    ulElement.style.listStyle = "none";
+    liElement.style.margin = "1rem";
+}
+
 function displayDogImageXHR(){
     // 1. Create a new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
@@ -65,39 +91,15 @@ function displayDogImageXHR(){
     // 4. This will be called after the response is received
     xhr.onload = function() {
         if (xhr.status != 200) {
-        // analyze HTTP status of the response
+            // analyze HTTP status of the response
         console.log(`Request Failed! ${xhr.responseText}`);
         } else {
             // show the result
             console.log(xhr.responseText);
             const dogData = JSON.parse(xhr.responseText);
             const dogImage = dogData.message;
-            console.log(dogImage);
 
-            const imageEl = document.createElement("img");
-            imageEl.src = dogImage;
-            imageEl.style.border = "10px solid #3ae374";
-            imageEl.style.width = "200px";
-            imageEl.style.margin = "0.5rem";
-
-            const divElement = document.getElementById("xhr-list-container");
-            const ulElement = document.getElementById("xhr-list");
-            const liElement = document.createElement("li");
-            divElement.appendChild(ulElement);
-            ulElement.appendChild(liElement);
-            liElement.appendChild(imageEl);
-            
-            divElement.style.display = "flex";
-            divElement.style.overflow = "hidden";
-            divElement.style.justifyItems = "center";
-            divElement.style.width = "50%";
-            ulElement.style.listStyle = "none";
-            liElement.style.fontSize = "0";
-            liElement.style.margin = "0";
-            
-            //ulElement.style.border = "1px solid blue";
-            //liElement.style.border = "1px solid green";
-
+            displayImage(dogImage);
         }
     };
 
@@ -108,7 +110,7 @@ function displayDogImageXHR(){
     xhr.onerror = function() {
         console.log('XHR Request Failed!');
 
-        errorMessage();
+        displayErrorMessage();
         
     };
 }
@@ -123,37 +125,14 @@ function displayDogImageAxios(){
         console.log(response.data.message);
         const dogImage = response.data.message;
 
-        const imageEl = document.createElement("img");
-            imageEl.src = dogImage;
-            imageEl.style.border = "10px solid #17c0eb";
-            imageEl.style.width = "250px";
-            imageEl.style.margin = "0.5rem";
-
-            const divElement = document.getElementById("axios-list-container");
-            const ulElement = document.getElementById("axios-list");
-            const liElement = document.createElement("li");
-            divElement.appendChild(ulElement);
-            ulElement.appendChild(liElement);
-            liElement.appendChild(imageEl);
-            
-            divElement.style.display = "flex";
-            divElement.style.overflow = "hidden";
-            divElement.style.justifyItems = "center";
-            divElement.style.width = "50%";
-            ulElement.style.listStyle = "none";
-            liElement.style.fontSize = "0";
-            liElement.style.margin = "0";
-            //divElement.style.border = "1px solid red";
-            //ulElement.style.border = "1px solid blue";
-            //liElement.style.border = "1px solid green";
-        
+        displayImage(dogImage);
       })
       .catch(function(error) {
         // handle error
         console.log(`Axios Request Failed! ${error}`);
 
-        errorMessage();
+        displayErrorMessage();
       });
-  }
-  displayDogImageAxios();
+}
+displayDogImageAxios();
   
